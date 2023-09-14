4ebb542506c0aeb8d75d85f28b5089b7563afff9
ksenia <ksy184694@gmail.com>
2023-09-14 20:18:41 +0300

    adding integrattion folder and tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,25 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 // Cypress.Commands.add('login', (email, password) => { ... })
+
+Cypress.Commands.add("login", (login, password) => {
+    cy.contains("Log in").click();
+    cy.get("#mail").type(login);
+    cy.get("#pass").type(password);
+    cy.contains("Submit").click();
+  });
+
+  Cypress.Commands.add("createNewBook", (title, author) => {
+    cy.contains("Add new").click();
+    cy.get("#title").type(title);
+    cy.get("#authors").type(author);
+    cy.contains("Submit").click();
+  });
+
+  Cypress.Commands.add("addBookToFavorite", (title, authors) => {
+    cy.contains("Add new").click();
+    cy.get("#title").type(title);
+    cy.get("#authors").type(authors);
+    cy.get("#favorite").click();
+    cy.contains("Submit").click();
+  });
